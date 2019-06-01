de1e8635baa5e07027e1d1fb3da886ef3c1403ba
Ivan Korotkin <ivan.korotkin@gmail.com>
2019-06-02 00:14:58 +0100

    Updated solver self test (now tests the solver in both single and double precision)

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
+rm -r build_single/
+mkdir build_single
+cd build_single
+cmake -DDAE_SINGLE=ON ..
+make -j 4
+ctest
+cd ..
+
 rm -r build/
 mkdir build
 cd build
--- test_single.sh
+++
@@ -1,6 +0,0 @@
-rm -r build/
-mkdir build
-cd build
-cmake -DDAE_SINGLE=ON ..
-make -j 4
-ctest
