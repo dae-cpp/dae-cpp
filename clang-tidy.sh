cf7b1dde450eea3a44f42ad0eed4b9d3f50b99a8
Ivan Korotkin <ivan.korotkin@gmail.com>
2019-06-21 14:42:55 +0100

    Add robertson example to CMakeLists and clang-tidy script

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -2,3 +2,4 @@
 clang-tidy src/*.cpp -- -I/opt/intel/mkl/include
 clang-tidy examples/perovskite/*.cpp -- -I/opt/intel/mkl/include
 clang-tidy examples/diffusion_2d/*.cpp -- -I/opt/intel/mkl/include
+clang-tidy examples/robertson/*.cpp -- -I/opt/intel/mkl/include
