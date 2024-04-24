ace8b69546c3ea2a8479721b9122c5eb2933e688
Ivan Korotkin <i.korotkin@soton.ac.uk>
2024-04-24 15:32:55 +0100

    Add static algebraic system test

--- tests/test_integration-algebraic.cpp
+++ tests/test_integration-algebraic.cpp
@@ -42,6 +42,9 @@ struct MyJacobian
 constexpr double abs_err_0{1e-14};
 constexpr double abs_err_1{1e-14};
 
+/*
+ * Time-dependent algebraic system
+ */
 TEST(Integration, Algebraic)
 {
     MyRHS rhs; // The vector-function object
@@ -66,6 +69,38 @@ TEST(Integration, Algebraic)
 
     EXPECT_LT(std::abs(my_system.sol.x.back()[0] - sin(t_end)), abs_err_0);
     EXPECT_LT(std::abs(my_system.sol.x.back()[1] - pow(cos(t_end), 2.0)), abs_err_1);
+    EXPECT_DOUBLE_EQ(my_system.sol.t.back(), t_end);
+}
+
+struct MyRHS_static
+{
+    void operator()(state_type &f, const state_type &x, const double t)
+    {
+        f[0] = 2.0 * x[0] - x[1];  // y = 2x
+        f[1] = x[0] * x[0] - x[1]; // y = x*x
+    }
+};
+
+/*
+ * Time-independent (static) algebraic system
+ */
+TEST(Integration, AlgebraicStatic)
+{
+    MyRHS_static rhs; // The vector-function object
+
+    state_vector x0{3.0, 3.0}; // Initial guess
+    double t_end{1.0};         // Solution interval: t = [0, t_end]
+
+    System my_system(MassMatrixZero(), rhs);
+
+    ASSERT_EQ(my_system.solve(x0, t_end), 0);
+    ASSERT_EQ(my_system.status, 0);
+
+    ASSERT_GT(my_system.sol.x.size(), 0);
+    ASSERT_GT(my_system.sol.t.size(), 0);
+
+    EXPECT_LT(std::abs(my_system.sol.x.back()[0] - 2.0), abs_err_0);
+    EXPECT_LT(std::abs(my_system.sol.x.back()[1] - 4.0), abs_err_1);
     EXPECT_DOUBLE_EQ(my_system.sol.t.back(), t_end);
 }
 
